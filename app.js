04d48571fbbd3d4268796e8df79fa161a8b36a32
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2020-09-23 13:51:29 +0300

    390920 13:51

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+
 const app = express();
 
-app.listen(5000, () => console.log("App has been started"));
+const PORT = config.get("port") || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUrl"), {});
+    app.listen(PORT, () =>
+      console.log(`App has been started on PORT ${PORT}...`)
+    );
+  } catch (e) {
+    console.log("Server Error", e.massage);
+    process.exit((code: 1));
+  }
+}
+
+start();
