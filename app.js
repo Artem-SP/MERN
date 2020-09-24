682f8963774552aa3f526859ed053b514323e0d3
Artem-SP <artem567gp@gmail.com>
2020-09-24 22:54:33 +0300

    240920 22:53 (tm 37:10) Works

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const config = require("config");
 const mongoose = require("mongoose");
 
 const app = express();
-app.use((fn: "/api/auth"), require("./routes/auth.routes"));
+//app.use((fn: "/api/auth"), require("./routes/auth.routes"));
 
 const PORT = config.get("port") || 5000;
 
 async function start() {
   try {
-    await mongoose.connect(config.get("mongoUrl"), {
+    await mongoose.connect(config.get("mongoUri"), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
