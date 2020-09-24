9c4efd6b3932c9e44c02d688d14bb2936c4d7af1
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2020-09-24 13:40:58 +0300

    240920 13:40 (tm 37:10) Errors in Codesandbox.io

--- routes/auth.routs.js
+++ routes/auth.routs.js
@@ -1,11 +1,33 @@
 const { Router } = require("express");
 const router = Router();
+const bcrypt = require('bcryptjs')
+const {check, validationResult} = ('express-validator')
 const User = require("../models/User");
 
 // /api/auth/register
-router.post("/register", async (req, res) => {
+router.post(
+  "/register",
+  [
+    check('email', 'Некоректный email').isEmail(),
+    check('pasword', 'Минимальная длина пароля 6 символов')
+  ]
+  async (req, res) => {
   try {
     const { email, pasword } = req.body;
+
+    const candidate = await User.findOne({email})
+
+    if (candidate) {
+      return res.status(400).json({ massage: 'Такой пользователь уже существует'})
+    }
+
+    const hashPasword = await bcrypt.hash(pasword, 12)
+    const user = new User({ email, pasword: hashPasword})
+
+    await user.sawe()
+
+    res.status(201).json({'Пользователь создан'})
+    
   } catch (e) {
     res.status(500).json({ massage: "Что-то пошло не такБ попробуйте снова" });
   }
