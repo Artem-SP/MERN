0cdafac8c7e35255bc6c3a8c95659da4b4404dc1
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2020-09-25 13:34:03 +0300

    250920 13:32 autRouter finished (tm 47:32) Errors in Codesandbox.io

--- routes/auth.routs.js
+++ routes/auth.routs.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const router = Router();
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const {check, validationResult} = ('express-validator')
 const User = require("../models/User");
+const config = require('config')
 
 // /api/auth/register
 router.post(
@@ -45,6 +47,11 @@ router.post(
 // /api/auth/register
 router.post(
   '/login',
+  [
+    check('email, 'Input correct email').normalizeEmail().isEmail(),
+    check('pasword', ' Input pasword').exists()
+
+  ]
   async (req, res) => {
     try {
       const errors = validationResult(req)
@@ -52,10 +59,30 @@ router.post(
       if (!errors.isEmpty) {
         return res.status(400).json({
           errors: errors.array(),
-          massage: 'Wrong registration data'
+          massage: 'Wrong conection data '
         })
       }
      
+      const{email, pasword} = req.body
+
+      const user = await User.findOne({ email })
+
+      if(!user) {
+        return res.status(400).json({'User not foud'})
+      }
+
+      const isMatch = await bicrybt.compare(pasword, userPasword)
+
+      if(!isMatch) {
+        return res.status(400).json({ message: 'Wrong pasword try again'})
+      }
+      const token = jwt.sign(
+        {userId: user.id},
+        config.get('jwtSecret'),
+        {expiresIn: '1h'}
+      )
+
+      res.json({token, userId: user.id})
 
     } catch (e) {
       res.status(500).json({ massage: "Something wrong" });
