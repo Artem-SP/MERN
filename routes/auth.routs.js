32955f3affe5c961debc39c82afff7c2fc5e490b
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2020-09-25 08:57:19 +0300

    initial commit

--- routes/auth.routs.js
+++ routes/auth.routs.js
@@ -6,19 +6,28 @@ const User = require("../models/User");
 
 // /api/auth/register
 router.post(
-  "/register",
+  '/register',
   [
-    check('email', 'Некоректный email').isEmail(),
-    check('pasword', 'Минимальная длина пароля 6 символов')
+    check('email', 'Wrong email').isEmail(),
+    check('pasword', 'Minimal length 6 dights')
+    .isLength(options: {min: 6} )
   ]
   async (req, res) => {
   try {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty) {
+      return res.status(400).json({
+        errors: errors.array(),
+        massage: 'Wrong registration data'
+      })
+    }
     const { email, pasword } = req.body;
 
     const candidate = await User.findOne({email})
 
     if (candidate) {
-      return res.status(400).json({ massage: 'Такой пользователь уже существует'})
+      return res.status(400).json({ massage: 'Such user is alredy existed'})
     }
 
     const hashPasword = await bcrypt.hash(pasword, 12)
@@ -26,14 +35,31 @@ router.post(
 
     await user.sawe()
 
-    res.status(201).json({'Пользователь создан'})
+    res.status(201).json({'User exist'})
     
   } catch (e) {
-    res.status(500).json({ massage: "Что-то пошло не такБ попробуйте снова" });
+    res.status(500).json({ massage: "Something wrong" });
   }
 });
 
 // /api/auth/register
-router.post("/login", async (req, res) => {});
+router.post(
+  '/login',
+  async (req, res) => {
+    try {
+      const errors = validationResult(req)
+  
+      if (!errors.isEmpty) {
+        return res.status(400).json({
+          errors: errors.array(),
+          massage: 'Wrong registration data'
+        })
+      }
+     
+
+    } catch (e) {
+      res.status(500).json({ massage: "Something wrong" });
+    }
+  });
 
 module.extorts = router;
