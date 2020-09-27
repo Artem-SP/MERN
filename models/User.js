6d2d139eddbe30337b93286de5dd49c442605ab6
Artem-SP <artem567gp@gmail.com>
2020-09-27 17:37:37 +0300

    270920 16:59 (tm 52:40) REACT added ERRRRS in auth

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const { Shema, model, Types } = require("mongoose");
-
-const schema = new Shema({
-  email: { type: String, required: true, unique: true },
-  pasword: { type: String, required: true },
-  links: [{ type: Types.ObjectID, ref: "Link" }]
-});
-
-module.exports = model((name: "User"), schema);
