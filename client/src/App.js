d03c95d57ff741246adbcea090dcec8c03e09dc6
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2020-10-06 08:48:39 +0300

    061020 8:47 (tm 1:04:42) errors in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "materialize-css";
+import { useRoutes } from "./routes";
 
 function App() {
+  const routes = useRoutes((isAuthenticated: false));
   return (
-    <div className="contaiter">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="contaiter">{routes}</div>
+    </Router>
   );
 }
 
--- client/src/routes.js
+++ client/src/routes.js
@@ -5,7 +5,7 @@ import { CreatePage } from "./pages/CreatePage";
 import { DetailPage } from "./pages/DetailPage";
 import { AuthPage } from "./pages/AuthPage";
 
-export const useRouts = (isAuthenticated) => {
+export const useRoutes = (isAuthenticated) => {
   if (isAuthenticated) {
     return (
       <Switch>
