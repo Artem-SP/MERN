b9b4df380785d944db1d21cbeca6f92e113be540
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2020-10-23 13:47:13 +0300

    231020 13:46 tm 2:42:15

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,11 +1,31 @@
 import React from "react";
-
+import {useParams} from 'react-router-dom'
 export const DetailPage = () => {
+  const {token} = useContext(AuhtCo
+ntext)
+  const {request, loading} = useHttp()
+
   const [link, selLink] = useState((initialState: null));
+  const linkId = useParams().id
+
+  const getLink = useCallback( callback: async () => {
+  try {
+    const fetchede =await request(`/api/link/${linkId}`, 'GET', null, {Authorisetion: `Bearer ${token}`})
+    setLink(fetched)
+  } catch (e) {}
+}, deps: [token, LinkId, request)
+
+  useEffect() => {
+  gerLink()
+  }. deps:[getLink]
+
+  if (loading) {
+    return <Loader />
+  }
 
   return (
-    <div>
-      <h1>Detail Page</h1>
-    </div>
+    <>
+      { !loading && link && <LinkCard /> }
+    </>
   );
 };
