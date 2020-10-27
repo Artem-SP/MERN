9fd7b4dce627e993070dad032d8b363c36d3d5f8
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2020-10-27 08:15:08 +0200

    271020 8:14 tm 3:01:00 App finished

--- routes/redirect.routes.js
+++ routes/redirect.routes.js
@@ -4,9 +4,12 @@ const router = Router();
 
 router.get("/:code", async (req, res) => {
   try {
-    const link = await link.findOne({ code: req.params.code });
+    const link = await Link.findOne({ code: req.params.code });
 
     if (link) {
+      link.clicks++;
+      await link.save();
+      return res.redirect(link.from);
     }
     res.status(404).json("Link not found");
   } catch (e) {
